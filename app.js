23535ece8ad96f5d42bf5cf8a80001a9b28f8c2b
Oleg Panikarovskiy <oleg.pnk@gmail.com>
2016-07-30 18:15:55 +0300

    add tg bot test

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+'use strict';
 const TelegramBot = require('node-telegram-bot-api');
 const mongoose = require('mongoose');
 const config = require('./config');
 
 const REPOSITORY_CONNECTION_STRING = config.get('repository:connectionString');
+const TELEGRAM_BOT_TOKEN = config.get('app:token');
+
+let bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect(REPOSITORY_CONNECTION_STRING).then(() => {
     console.info('Mongo connection success!');
+    bot.onText(/\/echo (.+)/, function (msg, match) {
+        var fromId = msg.from.id;
+        var resp = match[1];
+        bot.sendMessage(fromId, resp);
+    });
+
 });
